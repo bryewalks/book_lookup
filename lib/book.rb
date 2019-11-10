1a957eb2aa9cb90f16fdaeb50117f0f752fd7ce6
brye <bryewalks@gmail.com>
2019-11-09 18:31:24 -0600

    added test for book title when given and not given. added title to book initialize

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,7 @@
 class Book
+  attr_accessor :title
 
+  def initialize(input_options)
+    @title = input_options[:title] || "Unknown"
+  end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -4,12 +4,17 @@ describe Book do
   describe '#initialize' do
     context 'when title given' do
       it 'should use given title' do
-        
+        title = 'Fear and Loathing'
+        book = Book.new(title: title)
+        expect(book.title).to eq(title)
       end
     end
 
     context 'when no title given' do
-    
+      it 'should be unknown' do
+        book = Book.new(title: nil)
+        expect(book.title).to eq("Unknown")
+      end
     end
 
     context 'when authors given' do
