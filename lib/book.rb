79728be6ffe51688cf13dfecf4579e4ee0a605cd
brye <bryewalks@gmail.com>
2019-11-09 18:37:41 -0600

    added author to book class

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,8 @@
 class Book
-  attr_accessor :title
+  attr_accessor :title, :author
 
   def initialize(input_options)
-    @title = input_options[:title] || "Unknown"
+    @title = input_options[:title] || 'Unknown'
+    @author = input_options[:author] || 'Unknown'
   end
 end
