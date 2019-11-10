26deaaec6dc77fe0f427ca7c7c40a17b7c2efd68
brye <bryewalks@gmail.com>
2019-11-10 00:32:26 -0600

    added main program loop aswell as user choice methods. along with test for each

--- spec/book_lookup_spec.rb
+++ spec/book_lookup_spec.rb
@@ -29,4 +29,74 @@ describe BookLookup do
       expect { booklookup.display_books(book_array) }.to output("        Book #1        \nTitle: #{book.title}\nAuthor(s): #{book.author}\nPublisher: #{book.publisher}\n\n").to_stdout
     end
   end
+
+  describe '#continue?' do
+    context "user enters 'y'" do
+      it 'should return true' do
+        allow(booklookup).to receive(:user_input).and_return('y')
+        expect(booklookup.continue?).to eq(true)
+      end
+    end
+
+    context "user enters 'n'" do
+      it 'should return false' do
+        allow(booklookup).to receive(:user_input).and_return('n')
+        expect(booklookup.continue?).to eq(false)
+      end
+    end
+  end
+
+  describe '#add_to_saved?' do
+    it 'should ask use if they want to add selection' do
+      allow(booklookup).to receive(:user_input).and_return('n')
+      expect { booklookup.add_to_saved? }.to output("Would you like to add selections to reading list? y/n\n").to_stdout
+    end
+
+    it 'should call continue?' do
+      allow(booklookup).to receive(:user_input).and_return('n')
+      expect(booklookup).to receive(:continue?)
+      booklookup.add_to_saved?
+    end
+
+    context "user enters 'y'" do
+      it 'should return true' do
+        allow(booklookup).to receive(:user_input).and_return('y')
+        expect(booklookup.add_to_saved?).to eq(true)
+      end
+    end
+
+    context "user enters 'n'" do
+      it 'should return false' do
+        allow(booklookup).to receive(:user_input).and_return('n')
+        expect(booklookup.add_to_saved?).to eq(false)
+      end
+    end
+  end
+
+  describe '#search_again?' do
+    it 'should ask use if they want to search again' do
+      allow(booklookup).to receive(:user_input).and_return('n')
+      expect { booklookup.search_again? }.to output("Search again? y/n\n").to_stdout
+    end
+
+    it 'should call continue?' do
+      allow(booklookup).to receive(:user_input).and_return('n')
+      expect(booklookup).to receive(:continue?)
+      booklookup.search_again?
+    end
+
+    context "user enters 'y'" do
+      it 'should return true' do
+        allow(booklookup).to receive(:user_input).and_return('y')
+        expect(booklookup.search_again?).to eq(true)
+      end
+    end
+
+    context "user enters 'n'" do
+      it 'should return false' do
+        allow(booklookup).to receive(:user_input).and_return('n')
+        expect(booklookup.search_again?).to eq(false)
+      end
+    end
+  end
 end
