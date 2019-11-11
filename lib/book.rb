6df402345c42fb0e425dd9f398a6716a022bf628
brye <bryewalks@gmail.com>
2019-11-10 19:25:12 -0600

    added format_author method to book class to handle multiple authors given. changed author to authors to reflact more then one author.

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,17 @@
 class Book
-  attr_accessor :title, :author, :publisher
+  attr_accessor :title, :publisher, :authors
 
   def initialize(input_options)
     @title = input_options[:title] || 'Unknown'
-    @author = input_options[:author] || 'Unknown'
+    @authors = input_options[:authors] || ['Unknown']
     @publisher = input_options[:publisher] || 'Unknown'
+  end
+
+  def format_authors
+    if @authors.length == 1
+      @authors[0]
+    else @authors.length > 1
+      @authors.join(', ')
+    end
   end
 end
